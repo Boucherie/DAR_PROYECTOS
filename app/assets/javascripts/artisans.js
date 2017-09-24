45f315b79a7beba0a53f87cd401600a35319dfa1
Emily Boucher <emi.boucher@gmail.com>
2017-09-24 00:56:17 -0400

    got checkbox JS working thru DOM to display a test message. Changed community select div to a form. Formatted page for cleaner UI, moved eventual chart div to the bottom, set header in layout.

--- app/assets/javascripts/artisans.js
+++ app/assets/javascripts/artisans.js
@@ -17,4 +17,24 @@ document.addEventListener('DOMContentLoaded', function () {
       });
       console.log(orderSize);
     });
+
+  var names = document.querySelectorAll('input[type=checkbox]');
+  var displayBox = document.getElementById('community-display');
+  var communityForm = document.getElementById('community-select');
+
+  function setProductGroups() {
+    for (var i=0, len = names.length; i<len; i++) {
+      if (names[i].checked === true) {
+        displayBox.innerHTML = "test print" + names[i].id + "success!";
+
+      }
+    }
+  }
+
+  communityForm.addEventListener('click', function(e){
+    e.preventDefault();
+    setProductGroups();
+  });
+
+
 });
