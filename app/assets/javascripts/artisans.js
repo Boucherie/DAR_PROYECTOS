e31530352fa34db58405c07094ea6a796c8b6343
Emily Boucher <emi.boucher@gmail.com>
2017-10-05 16:44:19 -0400

    expanded to change values of checklist each time dropdown (product) changed; requred refactor in controller, changed json call to ruby hash, changed how ajax call selected all data; added global variables to select checboxes and linked them to database rows

--- app/assets/javascripts/artisans.js
+++ app/assets/javascripts/artisans.js
@@ -1,90 +1,121 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  chilete = document.querySelector('#chilete');
+  huanchao = document.querySelector('#huanchao');
+  huamachuco = document.querySelector('#huamachuco');
+  julcan = document.querySelector('#julcan');
+  limoncarlo1 = document.querySelector('#limon-carlo-rapidas');
+  limoncarlo2 = document.querySelector('#limon-carlo-lentas');
+  magaly = document.querySelector('#magaly');
+  milagro = document.querySelector('#milagro');
+  otrosierra = document.querySelector('#otrosierra');
+  palmeras= document.querySelector('#palmeras');
+  paypay= document.querySelector('#paypay');
+  progreso1= document.querySelector('#progreso-rapidas');
+  progreso2= document.querySelector('#progreso-lentas');
+  salitre1= document.querySelector('#salitre-rapidas');
+  salitre2= document.querySelector('#salitre-lentas');
+  sanpedrodelloc= document.querySelector('#san-pedro-de-lloc');
+  tembladera1= document.querySelector('#tembladera-rapidas');
+  tembladera2= document.querySelector('#tembladera-lentas');
+
   console.log( "ready!" );
     $.ajax({
       url: "http://localhost:3000/artisans",
       method: "GET",
       dataType: "json"
     }).done(function(data){
+      globalData = {};
       var products = [];
       data.forEach(function(item){
-        products.push(item.name).toString();
+        globalData[item.id] = item;
+        products.push(item.data.name).toString();
       });
+
       console.log(products);
       var orderSize = [];
       data.forEach(function(item){
         addOptionToDropDown(item);
-        orderSize.push(item.orderquantity).toFixed(0);
+        orderSize.push(item.data.orderquantity).toFixed(1);
       });
-      console.log(orderSize);
-      console.log(orderSize[3]);
-    });
-
-  var names = document.querySelectorAll('input[type=checkbox]');
-  var displayBox = document.getElementById('community-display');
-  var communityForm = document.getElementById('community-select');
-  var listDisplay = document.querySelector('ul');
-
-
-
-      // need to be able to add and remove element- add works, remove does not.
-
-        // 1. create and append element here- name, range slider (test version done)
-        // 2. else if call need to remove new element DONE
-        // 3. disable input until program is set- possible later feature
-        // 4. add dynamically populated dropdown
-        // 5. call function with MVP iterator here; call function containing AJAX call within MVP (hook csv list items into ajax dropdown selection)
-        // 6. have range sliders of each list item interact (max= item.value)
-
-    function addDisplayElement(el){
-      var listItem = document.createElement('li');
-      listItem.classList.add(el.id)
-      var rightSpan = document.createElement('span');
-      var testUpdate = document.createTextNode(el.id + ' selected - Rate of ' + el.value + ' units per month.');
-      var inputElement = document.createElement('input');
-      inputElement.type = 'range';
-      inputElement.min = "0"
-      inputElement.max = "1";
+      var options = document.querySelector('#optionsA');
+      options.addEventListener('change', function(option){
+        item = globalData[option.target.value];
+        chilete.value = Number(item.data['chilete']);
+        huanchao.value = Number(item.data['huanchao']);
+        huamachuco.value = Number(item.data['huamachuco']);
+        julcan.value = Number(item.data['julcan']);
+        limoncarlo1.value = Number(item.data['limoncarlo1']);
+        limoncarlo2.value = Number(item.data['limoncarlo2']);
+        magaly.value = Number(item.data['magaly']);
+        milagro.value = Number(item.data['milagro']);
+        otrosierra.value = Number(item.data['otrosierra']);
+        palmeras.value = Number(item.data['palmeras']);
+        paypay.value = Number(item.data['paypay']);
+        progreso1.value = Number(item.data['progreso1']);
+        progreso2.value = Number(item.data['progreso2']);
+        salitre1.value = Number(item.data['salitre1']);
+        salitre2.value = Number(item.data['salitre2']);
+        sanpedrodelloc.value = Number(item.data['sanpedrodelloc']);
+        tembladera1.value = Number(item.data['tembladera1']);
+        tembladera2.value = Number(item.data['tembladera2']);
+      })
 
 
-      rightSpan.appendChild(testUpdate);
-      listItem.appendChild(rightSpan);
-      listDisplay.appendChild(listItem);
-      listItem.appendChild(inputElement);
-    }
+// ------element list, interactive range component-------
+      var names = document.querySelectorAll('input[type=checkbox]');
+      var displayBox = document.getElementById('community-display');
+      var communityForm = document.getElementById('community-select');
+      var listDisplay = document.querySelector('ul');
 
-    function removeDisplayElement(el){
-      document.getElementsByClassName(el.id)[0].remove()
-    }
+      function addDisplayElement(el){
+        var listItem = document.createElement('li');
+        listItem.classList.add(el.id)
+        var rightSpan = document.createElement('span');
+        var testUpdate = document.createTextNode(el.id + ' selected - Rate of ' + el.value + ' units per month.');
+        var inputElement = document.createElement('input');
+        inputElement.type = 'range';
+        inputElement.min = "0"
+        inputElement.max = "1";
 
-    for (var i=0, len = names.length; i<len; i++) {
-      names[i].addEventListener('click', function(e){
-        console.log(i,e);
-        const el = e.currentTarget;
-        // e.preventDefault();
-        // setProductGroups();
-        if (el.checked === true){
-          addDisplayElement(el);
-        }else if (el.checked === false){
-          removeDisplayElement(el);
-        };
-      });
-    }
+        rightSpan.appendChild(testUpdate);
+        listItem.appendChild(rightSpan);
+        listDisplay.appendChild(listItem);
+        listItem.appendChild(inputElement);
+      }
+
+      function removeDisplayElement(el){
+        document.getElementsByClassName(el.id)[0].remove()
+      }
+
+      for (var i=0, len = names.length; i<len; i++) {
+        names[i].addEventListener('click', function(e){
+          console.log(i,e);
+          const el = e.currentTarget;
+          // e.preventDefault();
+          // setProductGroups();
+          if (el.checked === true){
+            addDisplayElement(el);
+          }else if (el.checked === false){
+            removeDisplayElement(el);
+          };
+        });
+      }
+
 
 
     function addOptionToDropDown(item){
       var options = document.querySelector('#optionsA');
       var newOption = document.createElement('option');
-      newOption.innerText = item.name;
-      newOption.value = item.orderquantity;
+      newOption.innerText = item.data.name;
+      newOption.value = item.id;
+      newOption.id = item.id;
       options.appendChild(newOption);
-      for (var i=0, len = newOption.length; i<len; i++) {
-        if (newOption[i].selected === true) {
+      // see options stackOverflow article to hook into dynamic list.
           // adding ranges to list items
-
-        };
       };
-    }
+    });
 
 
-});
+
+    });
--- app/controllers/artisans_controller.rb
+++ app/controllers/artisans_controller.rb
@@ -16,7 +16,24 @@ class ArtisansController < ApplicationController
       #   # end
       # }
       format.json {
-        render json: CSV.open(Rails.root.join('db', 'winter-2018-order.csv'), :headers => true).map { |x| x.to_h }.to_json
+        csv = CSV.open(Rails.root.join('db', 'winter-2018-order.csv'), :headers => true)
+        render json:
+          csv
+          .map {|row| row.to_h }
+          .map { |hash|
+            {
+              id: hash['name'].parameterize.gsub('-','_'),
+              data: hash
+            }
+          }
+
+          # .merge( {id: hash['name'].parameterize.gsub('-','_') } ) }
+        # render json: CSV.open(Rails.root.join('db', 'winter-2018-order.csv'), :headers => true)
+
+        # .map { |x| x.to_h }.map{|hash|
+          # hash['id'] =     [hash['code'].parameterize.gsub('-','_'), hash].merge({id: })}
+        # render json: CSV.open(Rails.root.join('db', 'winter-2018-order.csv'), :headers => true).map { |x| x.to_h }.map {|hash| }.to_h
+
         # CSV.foreach(Rails.root.join('db', 'tembla_capacity.csv')) do |row|
         #   puts row
           # use row (rails helper)
