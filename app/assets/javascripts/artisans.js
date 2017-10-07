f79b6dd81bbfb6870adb7adad45f6b48284c864b
Emily Boucher <emi.boucher@gmail.com>
2017-10-07 18:25:52 -0400

    added checkbox reset to option onchange event

--- app/assets/javascripts/artisans.js
+++ app/assets/javascripts/artisans.js
@@ -1,23 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
+  names = document.querySelectorAll('input[type=checkbox]');
+  var displayBox = document.getElementById('community-display');
+  var communityForm = document.getElementById('community-select');
+  var listDisplay = document.querySelector('ul');
 
-  chilete = document.querySelector('#chilete');
-  huanchao = document.querySelector('#huanchao');
-  huamachuco = document.querySelector('#huamachuco');
-  julcan = document.querySelector('#julcan');
-  limoncarlo1 = document.querySelector('#limon-carlo-rapidas');
-  limoncarlo2 = document.querySelector('#limon-carlo-lentas');
-  magaly = document.querySelector('#magaly');
-  milagro = document.querySelector('#milagro');
-  otrosierra = document.querySelector('#otrosierra');
-  palmeras= document.querySelector('#palmeras');
-  paypay= document.querySelector('#paypay');
-  progreso1= document.querySelector('#progreso-rapidas');
-  progreso2= document.querySelector('#progreso-lentas');
-  salitre1= document.querySelector('#salitre-rapidas');
-  salitre2= document.querySelector('#salitre-lentas');
-  sanpedrodelloc= document.querySelector('#san-pedro-de-lloc');
-  tembladera1= document.querySelector('#tembladera-rapidas');
-  tembladera2= document.querySelector('#tembladera-lentas');
+  const chilete = document.querySelector('#chilete');
+  const  huanchao = document.querySelector('#huanchao');
+  const  huamachuco = document.querySelector('#huamachuco');
+  const  julcan = document.querySelector('#julcan');
+  const  limoncarlo1 = document.querySelector('#limon-carlo-rapidas');
+  const  limoncarlo2 = document.querySelector('#limon-carlo-lentas');
+  const  magaly = document.querySelector('#magaly');
+  const  milagro = document.querySelector('#milagro');
+  const  otrosierra = document.querySelector('#otrosierra');
+  const  palmeras= document.querySelector('#palmeras');
+  const paypay= document.querySelector('#paypay');
+  const progreso1= document.querySelector('#progreso-rapidas');
+  const progreso2= document.querySelector('#progreso-lentas');
+  const salitre1= document.querySelector('#salitre-rapidas');
+  const salitre2= document.querySelector('#salitre-lentas');
+  const sanpedrodelloc= document.querySelector('#san-pedro-de-lloc');
+  const tembladera1= document.querySelector('#tembladera-rapidas');
+  const tembladera2= document.querySelector('#tembladera-lentas');
 
   console.log( "ready!" );
     $.ajax({
@@ -39,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
         orderSize.push(item.data.orderquantity).toFixed(1);
       });
       var options = document.querySelector('#optionsA');
+
       options.addEventListener('change', function(option){
         item = globalData[option.target.value];
         chilete.value = Number(item.data['chilete']);
@@ -59,63 +64,61 @@ document.addEventListener('DOMContentLoaded', function () {
         sanpedrodelloc.value = Number(item.data['sanpedrodelloc']);
         tembladera1.value = Number(item.data['tembladera1']);
         tembladera2.value = Number(item.data['tembladera2']);
+        while (listDisplay.firstChild){
+          listDisplay.removeChild( listDisplay.firstChild );
+        }
+        communityForm.reset();
+        // add function unchecking all checkboxes (for statement)
       })
 
 
 // ------element list, interactive range component-------
-      var names = document.querySelectorAll('input[type=checkbox]');
-      var displayBox = document.getElementById('community-display');
-      var communityForm = document.getElementById('community-select');
-      var listDisplay = document.querySelector('ul');
-
-      function addDisplayElement(el){
-        var listItem = document.createElement('li');
-        listItem.classList.add(el.id)
-        var rightSpan = document.createElement('span');
-        var testUpdate = document.createTextNode(el.id + ' selected - Rate of ' + el.value + ' units per month.');
-        var inputElement = document.createElement('input');
-        inputElement.type = 'range';
-        inputElement.min = "0"
-        inputElement.max = "1";
-
-        rightSpan.appendChild(testUpdate);
-        listItem.appendChild(rightSpan);
-        listDisplay.appendChild(listItem);
-        listItem.appendChild(inputElement);
-      }
-
-      function removeDisplayElement(el){
-        document.getElementsByClassName(el.id)[0].remove()
-      }
-
-      for (var i=0, len = names.length; i<len; i++) {
-        names[i].addEventListener('click', function(e){
-          console.log(i,e);
-          const el = e.currentTarget;
-          // e.preventDefault();
-          // setProductGroups();
-          if (el.checked === true){
-            addDisplayElement(el);
-          }else if (el.checked === false){
-            removeDisplayElement(el);
-          };
-        });
-      }
 
 
+  function addDisplayElement(el){
+    var listItem = document.createElement('li');
+    listItem.classList.add(el.id)
+    var rightSpan = document.createElement('span');
+    var testUpdate = document.createTextNode(el.id + ' selected - Rate of ' + el.value + ' units per month.');
+    var inputElement = document.createElement('input');
+    inputElement.type = 'range';
+    inputElement.min = "0"
+    inputElement.max = "1";
 
-    function addOptionToDropDown(item){
-      var options = document.querySelector('#optionsA');
-      var newOption = document.createElement('option');
-      newOption.innerText = item.data.name;
-      newOption.value = item.id;
-      newOption.id = item.id;
-      options.appendChild(newOption);
-      // see options stackOverflow article to hook into dynamic list.
-          // adding ranges to list items
+    rightSpan.appendChild(testUpdate);
+    listItem.appendChild(rightSpan);
+    listDisplay.appendChild(listItem);
+    listItem.appendChild(inputElement);
+  }
+
+  function removeDisplayElement(el){
+    document.getElementsByClassName(el.id)[0].remove()
+  }
+
+
+  for (var i=0, len = names.length; i<len; i++) {
+    names[i].addEventListener('click', function(e){
+      console.log(i,e);
+      const el = e.currentTarget;
+      // e.preventDefault();
+      // setProductGroups();
+      if (el.checked === true){
+        addDisplayElement(el);
+      }else if (el.checked === false){
+        removeDisplayElement(el);
       };
     });
+  }
 
-
-
-    });
+  function addOptionToDropDown(item){
+    var options = document.querySelector('#optionsA');
+    var newOption = document.createElement('option');
+    newOption.innerText = item.data.name;
+    newOption.value = item.id;
+    newOption.id = item.data['orderquantity'];
+    options.appendChild(newOption);
+    // see options stackOverflow article to hook into dynamic list.
+        // adding ranges to list items
+    };
+  });
+});
